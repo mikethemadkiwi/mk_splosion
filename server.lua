7ad10b6cfb72fe8bd31e23f40bbd56bcb3c76485
Mike The Madkiwi <mike@hildyard.ca>
2022-04-17 05:38:10 -0400

    some dude named mts tried to splode me. wasnt in city... but he tried HARD. this mod should solve that

--- server.lua
+++ server.lua
@@ -1,11 +1,10 @@
 numSplosions = {}
 local BlockedExplosions = {1, 2, 4, 5, 7, 8, 10, 17, 15, 25, 32, 33, 35, 36, 37, 38}
 local BannedExplosions = {53, 55, 56, 59}
-AddEventHandler(
-  "explosionEvent",
-  function(sender, ev)
+AddEventHandler("explosionEvent", function(sender, ev)
+    -----------------------------
     for _, v in ipairs(BlockedExplosions) do
-      if ev.explosionType == v then
+        if ev.explosionType == v then
         CancelEvent()
         if numSplosions[sender] == nil then
             numSplosions[sender] = 1
@@ -15,7 +14,21 @@ AddEventHandler(
         print('User ['..sender..'] has generated ['..numSplosions[sender]..'] explosions events. ( EVID:'..ev.explosionType..' )')
         -- throw in warning or ban scripts.
         return
-      end
+        end
     end
-  end
-)
+    -----------------------------
+    for _, v in ipairs(BannedExplosions) do
+        if ev.explosionType == v then
+            CancelEvent()
+            if numSplosions[sender] == nil then
+                numSplosions[sender] = 1
+            else
+                numSplosions[sender] = numSplosions[sender] + 1
+            end
+            print('User ['..sender..'] has generated ['..numSplosions[sender]..'] explosions events. ( EVID:'..ev.explosionType..' )')
+            DropPlayer(sender, 'those explosions are NOT events that just "happen".... get out. '
+            return
+        end
+    end
+    -----------------------------
+end)
